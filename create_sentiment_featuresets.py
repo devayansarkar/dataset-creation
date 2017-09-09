84adca1817fcfb0d9aa7bbaa5c2bd3a7094ae06c
Devayan Sarkar <devayansarkar@gmail.com>
2017-09-09 10:39:53 +0200

    creating lexicons

---
+++ create_sentiment_featuresets.py
@@ -0,0 +1,32 @@
+'''importing required modules'''
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import pickle
+from collections import Counter
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 10000000
+
+def create_lexicon(pos,neg):
+    lexicon = []
+    for fi in [pos,neg]:
+        with open(fi,'r') as f:
+            contents = f.readlines()
+            for l in contents[:hm_lines]:
+                all_words = word_tokenize(l.lower())
+                lexicon += list(all_words)
+
+    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+    w_counts= Counter(lexicon)
+
+    l2 = []
+    for w in w_counts:
+        if 1000 > w_counts[w] > 50:
+            l2.append(w)
+    print(len(l2))
+    return l2
+
+
